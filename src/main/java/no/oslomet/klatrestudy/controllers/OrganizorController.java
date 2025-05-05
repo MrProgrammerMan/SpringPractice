8643bceabf0a1809fcf8998a1de8218f0ddc5abd
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-05-05 12:37:04 +0200

    refactor: Remove unused import for RequestBody in OrganizorController

--- src/main/java/no/oslomet/klatrestudy/controllers/OrganizorController.java
+++ src/main/java/no/oslomet/klatrestudy/controllers/OrganizorController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import no.oslomet.klatrestudy.entities.Organizor;
 import no.oslomet.klatrestudy.services.OrganizorService;
